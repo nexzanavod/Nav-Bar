9a532f66d72d1ddba5941e923dcdf50c54628f20
nexzanavod <aanavod@gmail.com>
2021-04-18 21:33:07 +0530

    front end

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Header</header>
+
+      <Navbar />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+
+ return (
+  <div className="sidenav">
+   <ul>
+    <li>
+     <a href="/">Home</a>
+    </li>
+   </ul>
+  </div>
+ )
+}
+
+export default Navbar
